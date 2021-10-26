3e13a5d929095345921e33743dce32612801bfe0
Mirzazhar <azr_azr123@hotmail.com>
2021-10-27 04:14:20 +0500

    p,q & Zp

--- elgamal.go
+++ elgamal.go
@@ -1,6 +1,7 @@
 package elgamal
 
 import (
+	"crypto/rand"
 	"errors"
 	"math/big"
 )
@@ -21,4 +22,40 @@ type PublicKey struct {
 type PrivateKey struct {
 	PublicKey
 	X *big.Int
+}
+
+// Note : this section of code is taken from (https://github.com/ldinc/pqg).
+// Author of this code is "Drogunov Igor".
+// Gen emit <p,q,g>.
+// p = 2q + 1, p,q - safe primes
+// g - cyclic group generator Zp
+// performs n Miller-Rabin tests with 1 - 1/(4^n) probability false rate.
+// Gain n - bit width for integer & probability rang for MR.
+// It returns p, q, g and write error message.
+func Gen(n, probability int) (*big.Int, *big.Int, *big.Int, error) {
+	for {
+		q, err := rand.Prime(rand.Reader, n-1)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		t := new(big.Int).Mul(q, two)
+		p := new(big.Int).Add(t, one)
+		if p.ProbablyPrime(probability) {
+			for {
+				g, err := rand.Int(rand.Reader, p)
+				if err != nil {
+					return nil, nil, nil, err
+				}
+				b := new(big.Int).Exp(g, two, p)
+				if b.Cmp(one) == 0 {
+					continue
+				}
+				b = new(big.Int).Exp(g, q, p)
+				if b.Cmp(one) == 0 {
+					return p, q, g, nil
+				}
+			}
+		}
+	}
+	return nil, nil, nil, errors.New("can't emit <p,q,g>")
 }
